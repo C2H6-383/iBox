fd60c260e7502bf1154627a8165713a81cf1da18
C2H6-383 <ethan.finley@web.de>
2020-06-30 22:39:33 +0200

    fixed script execution

--- src/ibox.js
+++ src/ibox.js
@@ -116,7 +116,7 @@ global.ibox = class {
   }
 
   display(state) {
-    if (!ibox_utils.closeable(this.object_name) && state==false) {
+    if (!ibox_utils.closeable(this.object_name) && state == false) {
       this.fire("close_failed");
       return;
     }
@@ -150,7 +150,8 @@ global.ibox = class {
   }
 
   content_set(text) {
-    return (this.get_dom().querySelector(".content").innerHTML = text);
+    this.get_dom().querySelector(".content").innerHTML = text;
+    ibox_utils.exec_body_scripts(this.get_dom().querySelector(".content"));
   }
 
   content_get() {
@@ -158,7 +159,8 @@ global.ibox = class {
   }
 
   content_append(text) {
-    return (this.get_dom().querySelector(".content").innerHTML += text);
+    this.get_dom().querySelector(".content").innerHTML += text;
+    ibox_utils.exec_body_scripts(this.get_dom().querySelector(".content"));
   }
 
   content_clear() {
@@ -288,6 +290,63 @@ global.ibox_utils = class {
     outer.parentNode.removeChild(outer);
 
     return scrollbarWidth;
+  }
+
+  static exec_body_scripts(body_el) {
+    // Finds and executes scripts in a newly added element's body.
+    // Needed since innerHTML does not run scripts.
+    //
+    // Argument body_el is an element in the dom.
+
+    function nodeName(elem, name) {
+      return (
+        elem.nodeName && elem.nodeName.toUpperCase() === name.toUpperCase()
+      );
+    }
+
+    function evalScript(elem) {
+      var data = elem.text || elem.textContent || elem.innerHTML || "",
+        head =
+          document.getElementsByTagName("head")[0] || document.documentElement,
+        script = document.createElement("script");
+
+      script.type = "text/javascript";
+      try {
+        // doesn't work on ie...
+        script.appendChild(document.createTextNode(data));
+      } catch (e) {
+        // IE has funky script nodes
+        script.text = data;
+      }
+
+      head.insertBefore(script, head.firstChild);
+      head.removeChild(script);
+    }
+
+    // main section of function
+    var scripts = [],
+      script,
+      children_nodes = body_el.childNodes,
+      child,
+      i;
+
+    for (i = 0; children_nodes[i]; i++) {
+      child = children_nodes[i];
+      if (
+        nodeName(child, "script") &&
+        (!child.type || child.type.toLowerCase() === "text/javascript")
+      ) {
+        scripts.push(child);
+      }
+    }
+
+    for (i = 0; scripts[i]; i++) {
+      script = scripts[i];
+      if (script.parentNode) {
+        script.parentNode.removeChild(script);
+      }
+      evalScript(scripts[i]);
+    }
   }
 };
 
